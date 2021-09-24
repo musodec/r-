224cedf57a553d0e43682f2ccb16484421667166
Declan Murphy <declan@musimate.ie>
2021-09-24 17:32:51 +0100

    Transit WIP: -v verbose flag

--- r~.go
+++ r~.go
@@ -2,18 +2,49 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 	"path/filepath"
 )
 
+var verbose bool
+
+func init() {
+	flag.BoolVar(&verbose, "v", false, "Give more verbose output")
+}
+
 func main() {
+	flag.Parse()
 	log.SetFlags(0)
-	d, err := os.Getwd()
-	if err != nil {
-		log.Fatal("Getwd: " + err.Error())
+	log.Printf("NArgs = %d", flag.NArg())
+
+	switch flag.NArg() {
+	case 0:
+		d, err := os.Getwd()
+		if err != nil {
+			log.Fatal("Getwd: " + err.Error())
+		}
+		if verbose {
+			log.Printf("Working Directory: %s\n", d)
+		}
+	case 1:
+		d := flag.Arg(1)
+		if filepath.IsAbs(d) {
+			d = filepath.Clean(d)
+			fi, err := os.Stat(d)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if !fi.IsDir() {
+				log.Fatalf("%q not a directory", d)
+				log.Printf("Absolute dir %q", d)
+			}
+		}
+	default:
+		flag.Usage()
+		os.Exit(1)
 	}
-	log.Printf("Working Directory: %s\n", d)
 	g, err := filepath.Glob("*~")
 	if err == filepath.ErrBadPattern {
 		log.Fatalf("ErrBadPattern: %s\n", err.Error())
@@ -24,7 +55,9 @@ func main() {
 			log.Fatal("Stat: " + err.Error())
 		}
 		if !f.Mode().IsRegular() {
-			log.Printf("%s is not a regular file - skipping\n", p)
+			if verbose {
+				log.Printf("%s is not a regular file - skipping\n", p)
+			}
 			continue
 		}
 		err = os.Remove(p)
